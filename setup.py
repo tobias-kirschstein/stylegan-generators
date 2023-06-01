1d59ca9a3d135039923abd68e486737130b1644b
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2023-06-01 19:34:29 +0200

    Try

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ import setuptools
 if __name__ == "__main__":
     # Still necessary, otherwise we get a pip error
     setuptools.setup(include_package_data=True,
-                     package_data={'': ['*.cpp']},)
+                     # package_data={'': ['*.cpp']},
+                     )
