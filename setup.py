665b053fff790a96de14730c0e37af3df9dd9249
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2023-06-01 19:43:30 +0200

    Add .h files

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ import setuptools
 if __name__ == "__main__":
     # Still necessary, otherwise we get a pip error
     setuptools.setup(include_package_data=True,
-                     # Important to also install .cpp and .cu files that are in torch_utils/ops
-                     package_data={'': ['*.cpp', '*.cu']},
+                     # Important to also install .cpp/.h/.cu files that are in torch_utils/ops
+                     package_data={'': ['*.cpp', '*.cu', '*.h']},
                      )
