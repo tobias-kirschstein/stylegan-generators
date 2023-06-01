914b6e735ad045a07db074ccd8b00e6601fa404f
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2023-06-01 19:35:40 +0200

    Try

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ import setuptools
 if __name__ == "__main__":
     # Still necessary, otherwise we get a pip error
     setuptools.setup(include_package_data=True,
-                     # package_data={'': ['*.cpp']},
+                     package_data={'': ['*.cpp']},  # Important to also install .cpp files that are in torch_utils/ops
                      )
